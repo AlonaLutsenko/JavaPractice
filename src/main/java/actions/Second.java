798dfa9d0c4ba15f1695a9ef8c4f9a8a2a128d8c
AlonaLutsenko <alonalutsenkobb@gmail.com>
2021-07-22 00:25:14 +0300

    five

--- src/main/java/actions/Second.java
+++ src/main/java/actions/Second.java
@@ -1,11 +1,33 @@
 package actions;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-
-import java.util.concurrent.TimeUnit;
+import com.sun.org.glassfish.gmbal.Description;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Second {
+    private CreateNewsPage createNewsPage;
+    private OrderDetailsPage orderDetailsPage;
 
+    @BeforeMethod
+    public void signIn() {
+        User user = UserRepository.get().temporary();
+        orderDetailsPage = loadApplication()
+                .signIn()
+                .getManualLoginComponent()
+                .successfullyLogin(user)
+                .navigateMenuUBSCourier();
+    }
+
+    @Test(testName = "GC-", description = "GC-")
+    @Description("GC-")
+    public void addAddress() {
+        UserAddress userAddress = new UserAddress(AddAddressPopupLocators.CITY_KIEV,"Sadova", "Kiev", 1, "1", 2);
+        //  new AddAddressPopupComponent(driver).inputDistrict(userAddress);
+    }
+
+    @AfterMethod
+    public void signOut() {
+        createNewsPage.signOut();
+    }
 }
