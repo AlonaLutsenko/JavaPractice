ba3fdb9f512f02a052b07683bedcc04247c9a3b4
AlonaLutsenko <alonalutsenkobb@gmail.com>
2021-07-22 00:30:41 +0300

    six

--- src/main/java/actions/Seven.java
+++ src/main/java/actions/Seven.java
@@ -1,4 +1,15 @@
 package actions;
 
+import org.testng.annotations.BeforeMethod;
+
 public class Seven {
+    @BeforeMethod
+    public void signIn() {
+        User user = UserRepository.get().temporary();
+        orderDetailsPage = loadApplication()
+                .signIn()
+                .getManualLoginComponent()
+                .successfullyLogin(user)
+                .navigateMenuUBSCourier();
+    }
 }
