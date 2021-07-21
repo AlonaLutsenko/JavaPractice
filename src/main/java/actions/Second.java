d7a3b16f205fa4e9cffce822dc73cec5617bd3fb
AlonaLutsenko <alonalutsenkobb@gmail.com>
2021-07-22 00:34:27 +0300

    seven

--- src/main/java/actions/Second.java
+++ src/main/java/actions/Second.java
@@ -6,28 +6,4 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Second {
-    private CreateNewsPage createNewsPage;
-    private OrderDetailsPage orderDetailsPage;
-
-    @BeforeMethod
-    public void signIn() {
-        User user = UserRepository.get().temporary();
-        orderDetailsPage = loadApplication()
-                .signIn()
-                .getManualLoginComponent()
-                .successfullyLogin(user)
-                .navigateMenuUBSCourier();
-    }
-
-    @Test(testName = "GC-", description = "GC-")
-    @Description("GC-")
-    public void addAddress() {
-        UserAddress userAddress = new UserAddress(AddAddressPopupLocators.CITY_KIEV,"Sadova", "Kiev", 1, "1", 2);
-        //  new AddAddressPopupComponent(driver).inputDistrict(userAddress);
-    }
-
-    @AfterMethod
-    public void signOut() {
-        createNewsPage.signOut();
-    }
 }
