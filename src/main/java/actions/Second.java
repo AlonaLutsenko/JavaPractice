a965a87a1a3b6730e552a957fb320bdd7a45ac10
Alona_Lutsenko <71566280+AlonaLutsenko@users.noreply.github.com>
2021-12-24 13:45:21 +0200

    Delete Second.java

--- src/main/java/actions/Second.java
+++
@@ -1,9 +0,0 @@
-package actions;
-
-import com.sun.org.glassfish.gmbal.Description;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-public class Second {
-}
